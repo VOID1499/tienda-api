46f7dd2fe7dec30093ed66d981cae1ad17d15b9c
erick <erick.olivares.gonzalez@gmail.com>
2025-09-20 21:48:32 -0300

    se añade momentjs y cambios en el envio de correo

--- src/utils/document-service-middlewares.js
+++ src/utils/document-service-middlewares.js
@@ -1,159 +1,165 @@
+const { Result } = require("pg");
+const orden = require("../api/orden/controllers/orden.js");
 const formatearPesosCLP = require("./formatCurrency.js");
 const colors = require("colors");
-const pageTypes = ["api::article.article"];
-const pageActions = ["create", "update"];
+const pageTypes = ["api::orden.orden"];
+const pageActions = ["update"];
 const sendEmailActions = ["publish"];
-
 
 const enviarEmailConfirmacionDePago = () => {
   return async (context, next) => {
-    
-    const result = await next(); //
+    const { uid, action, params } = context;
 
-    if (context.action == 'update' && context.uid == 'api::orden.orden') {
-      if(result.estado == 'pagada'){
-
+    try {
+      // Solo si es un update de orden y viene el campo estado
+      if (pageTypes.includes(uid) && pageActions.includes(action) && params.data.estado) {
         
-
-        try {
-           const orden = await strapi.documents("api::orden.orden").findOne({
-            documentId:result.documentId,
+        // Obtener estado anterior antes de ejecutar el update
+        const previous = await strapi.documents("api::orden.orden").findOne({
+          documentId:params.documentId,
             populate:{
-            metodos_de_envio:{
-              fields:["nombre"]
-            },
-            metodos_de_pago:{
-              fields:["nombre"]
-            },
-            orden_detalles:{
-              fields:["cantidad","precio","subtotal","snapshot_info_producto"]
+              metodos_de_envio:{
+                fields:["nombre"]
+              },
+              metodos_de_pago:{
+                fields:["nombre"]
+              },
+              orden_detalles:{
+                fields:["cantidad","precio","subtotal","snapshot_info_producto"]
+              }
             }
-            }
-          })
-      
-          console.log(colors.bgBlue(`Enviando correo de confirmacion de compra a ${result.email}`));
+        });
+
+        // Ejecuta el update
+        const result = await next();
+
+        // Comparar estado y disparar correo solo si cambió a 'pagada'
+        if (previous.estado !== result.estado && result.estado === 'pagada') {
+          
+          console.log(previous)
           await strapi.plugin('email').service('email').send({
-            to: 'erick.olivares.gonzalez@gmail.com',
+            to: previous.email,
             bcc: 'erick.olivares.gonzalez@gmail.com',
             subject: `Confirmacion de tu compra en Michisytarot`,
             text: `
               Michisytarot
 
-              Muchas gracias por tu compra ${orden.nombre.toUpperCase()} 😺
+              Muchas gracias por tu compra ${previous.nombre.toUpperCase()} 😺
 
-              Fecha de pago: ${orden.fecha_de_pago}
-              Orden ID: ${orden.documentId}
+              Fecha de pago: ${previous.fecha_de_pago}
+              Orden ID: ${previous.documentId}
 
               Información de envío:
-              ${orden.nombre} ${orden.apellidos}
-              ${orden.direccion}
-              Telefono: ${orden.Telefono}
+              ${previous.nombre} ${previous.apellidos}
+              ${previous.direccion}
+              Telefono: ${previous.telefono}
 
-              Método de pago: ${orden.metodos_de_pago.nombre}
-              Método de envío: ${orden.metodos_de_envio.nombre}
+              Método de pago: ${previous.metodos_de_pago.nombre}
+              Método de envío: ${previous.metodos_de_envio.nombre}
 
               Productos:
-              ${orden.orden_detalles.map(item => `${item.snapshot_info_producto} * ${item.cantidad}`).join('\n')}
+              ${previous.orden_detalles.map(item => `${item.snapshot_info_producto} * ${item.cantidad}`).join('\n')}
 
-              Total de la compra CLP ${orden.total}
+              Total de la compra CLP ${previous.total}
             `
 ,
             html: `
              
 
-		 <table width="100%" cellpadding="0" cellspacing="0" style="background: #f9f9f9; padding: 20px; font-family: sans-serif;">
-              <tr>
-                <td align="center">
-                  <table width="50%" cellpadding="0" cellspacing="5" style=" box-sizing: border-box;">
+              <table width="100%" cellpadding="0" cellspacing="0" style="background: #f9f9f9; padding: 20px; font-family: sans-serif;">
+                        <tr>
+                          <td align="center">
+                            <table width="50%" cellpadding="0" cellspacing="5" style=" box-sizing: border-box;">
 
-                    <tr>
-                      <td style="padding: 0 20px; text-align: right; box-sizing: border-box;">
-                        <h1 style="margin: 0; box-sizing: border-box;">Michisytarot</h1>
-                      </td>
-                    </tr>
+                              <tr>
+                                <td style="padding: 0 20px; text-align: right; box-sizing: border-box;">
+                                  <h1 style="margin: 0; box-sizing: border-box;">Michisytarot</h1>
+                                </td>
+                              </tr>
 
-                    <tr>
-                      <td style="padding: 5px; background: black; box-sizing: border-box;">
-                        <h2 style="color: white; margin: 0; box-sizing: border-box;">Muchas gracias por tu compra ${orden.nombre.toUpperCase()} 😺</h2>
-                      </td>
-                    </tr>
-                   
+                              <tr>
+                                <td style="padding: 5px; background: black; box-sizing: border-box;">
+                                  <h2 style="color: white; margin: 0; box-sizing: border-box;">Muchas gracias por tu compra ${previous.nombre.toUpperCase()} 😺</h2>
+                                </td>
+                              </tr>
+                            
 
-                    <tr>
-                      <td style="padding: 5px; box-sizing: border-box;">
-                        <span style="display:block; box-sizing: border-box;">Fecha de pago ${orden.fecha_de_pago}</span>
-                        <h3 style="margin: 0; box-sizing: border-box;">Orden ID : ${orden.documentId}</h3>
-                      </td>
-                    </tr>
+                              <tr>
+                                <td style="padding: 5px; box-sizing: border-box;">
+                                  <span style="display:block; box-sizing: border-box;">Fecha de pago ${previous.fecha_de_pago}</span>
+                                  <h3 style="margin: 0; box-sizing: border-box;">Orden ID : ${previous.documentId}</h3>
+                                </td>
+                              </tr>
 
-                    <tr>
-                      <td style="padding: 5px; box-sizing: border-box;">
-                        <h3 style="margin: 0; box-sizing: border-box;">Información de envío</h3>
-                        <p style="margin: 0;">
-                            ${orden.nombre} ${orden.apellidos} <br>
-                            ${orden.direccion} <br>
-                            Telefono ${orden.Telefono}
+                              <tr>
+                                <td style="padding: 5px; box-sizing: border-box;">
+                                  <h3 style="margin: 0; box-sizing: border-box;">Información de envío</h3>
+                                  <p style="margin: 0;">
+                                      ${previous.nombre} ${previous.apellidos} <br>
+                                      ${previous.direccion} <br>
+                                      Telefono ${previous.telefono}
 
-                        </p>
-                      </td>
-                    </tr>
+                                  </p>
+                                </td>
+                              </tr>
 
-                    <tr>
-                      <td style="padding: 5px; box-sizing: border-box;">
-                        <h3 style="margin: 0; box-sizing: border-box;">Método de pago</h3>
-                        <p style="margin: 0;">${orden.metodos_de_pago.nombre}</p>
-                      </td>
-                    </tr>
+                              <tr>
+                                <td style="padding: 5px; box-sizing: border-box;">
+                                  <h3 style="margin: 0; box-sizing: border-box;">Método de pago</h3>
+                                  <p style="margin: 0;">${previous.metodos_de_pago.nombre}</p>
+                                </td>
+                              </tr>
 
-                    <tr>
-                      <td style="padding: 5px; box-sizing: border-box;">
-                        <h3 style="margin: 0; box-sizing: border-box;">Método de envío</h3>
-                        <p style="margin: 0;">${orden.metodos_de_envio.nombre}</p>
-                      </td>
-                    </tr>
+                              <tr>
+                                <td style="padding: 5px; box-sizing: border-box;">
+                                  <h3 style="margin: 0; box-sizing: border-box;">Método de envío</h3>
+                                  <p style="margin: 0;">${previous.metodos_de_envio.nombre}</p>
+                                </td>
+                              </tr>
 
-                    <tr>
-                      <td style="padding: 5px; background: black; box-sizing: border-box;">
-                        <h3 style="color: white; margin: 0; box-sizing: border-box;">Total de la compra CLP ${formatearPesosCLP(orden.total)}</h3>
-                      </td>
-                    </tr>
+                              <tr>
+                                <td style="padding: 5px; background: black; box-sizing: border-box;">
+                                  <h3 style="color: white; margin: 0; box-sizing: border-box;">Total de la compra CLP ${formatearPesosCLP(previous.total)}</h3>
+                                </td>
+                              </tr>
 
-                    <td style="padding: 5px;  box-sizing: border-box;">
-                      ${
-                        orden.orden_detalles
-                          .map(item => `
-                            <h4 style="margin: 0; box-sizing: border-box; border-bottom: 1px solid black;">
-                              ${item.snapshot_info_producto} * ${item.cantidad}
-                              <span style="float: right;">
-                                c/u ${formatearPesosCLP(item.precio)} 
-                              </span>
-                            </h4>
-                          `)
-                          .join('')
-                      }
-                    </td>
+                              <td style="padding: 5px;  box-sizing: border-box;">
+                                ${
+                                  previous.orden_detalles
+                                    .map(item => `
+                                      <h4 style="margin: 0; box-sizing: border-box; border-bottom: 1px solid black;">
+                                        ${item.snapshot_info_producto} * ${item.cantidad}
+                                        <span style="float: right;">
+                                          c/u ${formatearPesosCLP(item.precio)} 
+                                        </span>
+                                      </h4>
+                                    `)
+                                    .join('')
+                                }
+                              </td>
 
-                  </table>
-                </td>
-              </tr>
-            </table>
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
 
             `,
           });
-        } catch (err) {
-          console.log(colors.bgRed(`Error al enviar correo de confirmacion de compra ${result.email} ${err}`));
+        
         }
 
-         
-
+        return result;
       }
+
+      // Si no es update de estado, solo ejecuta next()
+      return await next();
+
+    } catch (error) {
+      console.error('Error al enviar correo de confirmacion de compra:', error);
+      // No relanzamos el error para no afectar el flujo de Strapi
+      return await next();
     }
-
-
-    return result;
   };
-};
-
-
+}
 
 module.exports = { enviarEmailConfirmacionDePago }
